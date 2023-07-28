3eae0bbba3dfd93f3a6e682ba407072e312891e3
Alexis Bocanegra <alexisbocanegra14@gmail.com>
2023-07-27 20:38:58 -0500

    Added routes for api and html

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const html_routes = require('./routes/html-routes')
-const api_routes = require('./routes/api-routes')
+const html_routes = require('./routes/routes')
+const api_routes = require('./routes/html-routes.js')
 const PORT = process.env.PORT || 3001;
-// dynamically set the port
 const app = express();
 
-// Express middleware will always run the operation in the order from top to bottom "order matters"
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
